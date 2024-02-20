10f5417cb259c53b4e280419616c954ca9f49cb8
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-02-20 22:22:36 +0900

    Initial commit

---
+++ src/test/java/com/gdsc/safesync/SafeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.safesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
